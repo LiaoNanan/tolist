a48c67c68657f69e7876a2c60077fa03da514840
LIAO <82252590+LiaoNanan@users.noreply.github.com>
2021-07-28 21:59:45 +0800

    Delete src directory

--- src/utils/localStorageUtil.ts
+++
@@ -1,7 +0,0 @@
-export function saveArray(key: string, value: []): void {
-    localStorage.setItem(key, JSON.stringify(value));
-}
-
-export function readArray(key: string): [] {
-    return JSON.parse(localStorage.getItem(key) || '[]');
-}
